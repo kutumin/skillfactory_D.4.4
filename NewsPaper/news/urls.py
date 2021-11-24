589abc5b306e4eb2b3238e7b57ac6bc4e3d26c58
kutumin <kutumin@gmail.com>
2021-11-24 15:07:49 +0300

    123

--- NewsPaper/news/urls.py
+++ NewsPaper/news/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import PostDetail, PostSearch, PostListNews
+from .views import PostDetail, PostSearch, PostListNews, PostAdd, ProductUpdateView
 
 urlpatterns = [
     path('', PostListNews.as_view()),
-    path('<int:pk>', PostDetail.as_view()),
+    path('<int:pk>', PostDetail.as_view(), name='post_detail'),
     path ('search', PostSearch.as_view()),
+    path ('add', PostAdd.as_view()),
+    path('<int:pk>/edit', ProductUpdateView.as_view(), name='update_post_detail'),
 ]
--- NewsPaper/news/views.py
+++ NewsPaper/news/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, UpdateView, DeleteView
 from .models import Post, PostCategory
 from django.core.paginator import Paginator
 from django.views import View 
@@ -19,6 +19,28 @@ class PostDetail(DetailView):
     context_object_name = 'news'
     queryset = Post.objects.filter(post_type='news')
 
+class PostDetailEdit(DetailView):
+    model = Post
+    template_name = 'details_news.html' 
+    context_object_name = 'news'
+    queryset = Post.objects.filter(post_type='news')
+
+class ProductUpdateView(UpdateView):
+    template_name = 'details_news.html'
+    form_class = PostForm
+ 
+    def get_object(self, **kwargs):
+        id = self.kwargs.get('pk')
+        return Post.objects.get(pk=id)
+ 
+ 
+# дженерик для удаления товара
+class ProductDeleteView(DeleteView):
+    template_name = 'sample_app/product_delete.html'
+    queryset = Post.objects.all()
+    success_url = '/products/'
+
+
 class PostSearch(ListView):
     model = Post
     template_name = 'search_news.html' 
@@ -33,6 +55,28 @@ class PostSearch(ListView):
         context['form'] = PostForm()
         return context
  
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST) 
+        
+        if form.is_valid(): 
+            form.save()
+ 
+        return super().get(request, *args, **kwargs)
+
+class PostAdd(ListView):
+    model = Post
+    template_name = 'add_news.html' 
+    context_object_name = 'news'
+    paginate_by = 1
+    form_class = PostForm
+
+    def get_context_data(self, **kwargs): 
+        context = super().get_context_data(**kwargs)
+        context['filter'] = PostFilter(self.request.GET, queryset=self.get_queryset())
+        context['PostCategory'] = PostCategory.objects.all()
+        context['form'] = PostForm()
+        return context
+
     def post(self, request, *args, **kwargs):
         form = self.form_class(request.POST) 
         

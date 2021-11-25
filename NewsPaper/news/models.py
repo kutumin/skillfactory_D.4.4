7b56d9a4ef99f385c2ee0931213ab4889fdb70d7
Evgeny <kutumin@gmail.com>
2021-11-25 11:57:22 +0300

    12

--- NewsPaper/news/models.py
+++ NewsPaper/news/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models import Sum
 
+# Test
+
 class Author(models.Model):
     full_name = models.CharField(max_length = 255)
     user = models.OneToOneField(User,on_delete = models.CASCADE)
